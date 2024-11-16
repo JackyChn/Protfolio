f2a3173867afb3f451d16462e6e8211a6917e2a5
JackyChn <ccc1020048076@gmail.com>
2024-11-16 21:01:42 +1030

    Update: background modified

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -22,10 +22,10 @@ export default function Hero() {
       </div>
 
       {/* Background */}
-      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2] relative flex items-center justify-center">
-        {/* Radial gradient for the container to give a faded look */}
+      <div className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.05] bg-grid-black/[0.2] absolute top-0 left-0 flex items-center justify-center">
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white-100 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-        <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8"></p>
+        {/* Add any sentences within <p> to show upon the grid */}
+        <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-100 to-neutral-900 py-8"></p>
       </div>
 
       <div className="flex justify-center relative my-20 z-10">
