9459f6fc1f02a8b3497ee1d09108ae8c832aeefe
JackyChn <ccc1020048076@gmail.com>
2024-11-16 23:23:08 +1030

    Add: grid bg style description render

--- app/_components/ui/BentoGrid.tsx
+++ app/_components/ui/BentoGrid.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@/app/_lib/utils";
+import { BackgroundGradientAnimation } from "./GradientBackGround";
 
 export const BentoGrid = ({
   className,
@@ -51,6 +52,7 @@ export const BentoGridItem = ({
           "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(76,76,190,1) 66%, rgba(0,212,255,1) 100%)",
       }}
     >
+      {/* id === 6 */}
       <div className={`${id === 6 && "flex justify-center"} h-full`}>
         <div className="w-full h-full absolute">
           {img && (
@@ -61,14 +63,46 @@ export const BentoGridItem = ({
             />
           )}
         </div>
+
+        {/* id === 5 */}
+        <div
+          className={`absolute right-0 -bottom-5 ${
+            id === 5 && "w-full opacity-80"
+          } `}
+        >
+          {spareImg && (
+            <img
+              src={spareImg}
+              alt={spareImg}
+              // width={220}
+              className="object-cover object-center w-full h-full"
+            />
+          )}
+        </div>
+
+        {/* id === 6 */}
+        {id === 6 && (
+          <BackgroundGradientAnimation>
+            <div className="absolute z-50 max-h-10 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl"></div>
+          </BackgroundGradientAnimation>
+        )}
+      </div>
+
+      {/* render description */}
+      <div
+        className={cn(
+          titleClassName,
+          "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
+        )}
+      >
+        <div className="font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300">
+          {description}
+        </div>
       </div>
 
       <div className="group-hover/bento:translate-x-2 transition duration-200">
         <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
           {title}
-        </div>
-        <div className="font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300">
-          {description}
         </div>
       </div>
     </div>
