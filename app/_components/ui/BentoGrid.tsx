1f0d6ae6124f9ed45e9c166a4b3a2dc5b586d430
JackyChn <ccc1020048076@gmail.com>
2024-11-17 16:05:07 +1030

    Add: description & title for each card

--- app/_components/ui/BentoGrid.tsx
+++ app/_components/ui/BentoGrid.tsx
@@ -52,7 +52,6 @@ export const BentoGridItem = ({
           "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(76,76,190,1) 66%, rgba(0,212,255,1) 100%)",
       }}
     >
-      {/* id === 6 */}
       <div className={`${id === 6 && "flex justify-center"} h-full`}>
         <div className="w-full h-full absolute">
           {img && (
@@ -64,7 +63,7 @@ export const BentoGridItem = ({
           )}
         </div>
 
-        {/* id === 5 */}
+        {/* id === 5 then render*/}
         <div
           className={`absolute right-0 -bottom-5 ${
             id === 5 && "w-full opacity-80"
@@ -80,7 +79,7 @@ export const BentoGridItem = ({
           )}
         </div>
 
-        {/* id === 6 */}
+        {/* id === 6 then render animation bg*/}
         {id === 6 && (
           <BackgroundGradientAnimation>
             <div className="absolute z-50 max-h-10 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl"></div>
@@ -88,20 +87,19 @@ export const BentoGridItem = ({
         )}
       </div>
 
-      {/* render description */}
+      {/* render description for every card*/}
       <div
         className={cn(
           titleClassName,
           "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
         )}
       >
-        <div className="font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300">
+        <div className="font-sans font-extralight md:max-w-32 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10">
           {description}
         </div>
-      </div>
-
-      <div className="group-hover/bento:translate-x-2 transition duration-200">
-        <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
+        <div
+          className={`font-sans text-lg lg:text-3xl max-w-96 font-bold text-white-200 z-10`}
+        >
           {title}
         </div>
       </div>
