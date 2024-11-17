db52b77ed848e0e67367a651683db7135a75a782
JackyChn <ccc1020048076@gmail.com>
2024-11-17 22:36:19 +1030

    Update: Floating Navbar

--- app/_components/Projects.tsx
+++ app/_components/Projects.tsx
@@ -13,7 +13,7 @@ export default function Projects() {
   return (
     <>
       {isClient && (
-        <div className="py-20">
+        <div className="py-20" id="projects">
           <h1 className="heading">
             A small selection of{" "}
             <span className="text-purple">recent projects</span>
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Hero from "./_components/Hero";
 import { FloatingNav } from "./_components/ui/FloatingNav";
-import { FaHome } from "react-icons/fa";
+import { navItems } from "@/data";
 import Grid from "../app/_components/Grid";
 import Projects from "./_components/Projects";
 
@@ -8,9 +8,7 @@ export default function Home() {
   return (
     <main className="relative bg-black-100 text-white-200 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav
-          navItems={[{ name: "Home", link: "/", icon: <FaHome /> }]}
-        />
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         <Projects />
