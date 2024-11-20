cbb5ecc6113bab0192f2cd3c4b77b71557c9c8b5
JackyChn <ccc1020048076@gmail.com>
2024-11-20 18:02:37 +1030

    Complete: version1

--- app/_components/ui/ReactQueryWrapper.tsx
+++ app/_components/ui/ReactQueryWrapper.tsx
@@ -2,7 +2,7 @@
 
 import React, { ReactNode } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function ReactQueryWrapper({ children }: { children: ReactNode }) {
   const queryClient = new QueryClient({
@@ -16,8 +16,7 @@ function ReactQueryWrapper({ children }: { children: ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      {/* TODO: remove after dev complete */}
-      <ReactQueryDevtools initialIsOpen={true} />
+      {/* <ReactQueryDevtools initialIsOpen={true} /> */}
     </QueryClientProvider>
   );
 }
