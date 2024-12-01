0f8078ee08b771214982b44dd9caafe3ca90a5ea
JackyChn <ccc1020048076@gmail.com>
2024-12-02 00:14:14 +1030

    Fix: try4-leave only Nav and Footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import Hero from "./_components/Hero";
+// import Hero from "./_components/Hero";
 import { FloatingNav } from "./_components/ui/FloatingNav";
 import { navItems } from "@/data";
-import Grid from "../app/_components/Grid";
-import Projects from "./_components/Projects";
-import Clients from "./_components/Clients";
-import Experience from "./_components/Experience";
+// import Grid from "../app/_components/Grid";
+// import Projects from "./_components/Projects";
+// import Clients from "./_components/Clients";
+// import Experience from "./_components/Experience";
 import Footer from "./_components/Footer";
 
 export default function Home() {
@@ -12,11 +12,11 @@ export default function Home() {
     <main className="relative bg-black-100 text-white-200 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
-        <Hero />
+        {/* <Hero />
         <Grid />
         <Projects />
         <Clients />
-        <Experience />
+        <Experience /> */}
         <Footer />
       </div>
     </main>
