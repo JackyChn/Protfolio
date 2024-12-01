252bd8fec42520824470b3a43e8736181775d6c1
JackyChn <ccc1020048076@gmail.com>
2024-12-02 00:44:45 +1030

    Update: handleCopy problem try2

--- app/_components/Grid.tsx
+++ app/_components/Grid.tsx
@@ -6,7 +6,7 @@ export default function Grid() {
   return (
     <section id="about">
       <BentoGrid className="w-full pt-20 pb-0">
-        {/* {gridItems.map((item, i) => (
+        {gridItems.map((item, i) => (
           <BentoGridItem
             id={item.id}
             key={i}
@@ -18,7 +18,7 @@ export default function Grid() {
             titleClassName={item.titleClassName}
             spareImg={item.spareImg}
           />
-        ))} */}
+        ))}
       </BentoGrid>
     </section>
   );
