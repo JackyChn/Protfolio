78f616a1a0b25e674381a61acadf00df4066c48f
JackyChn <ccc1020048076@gmail.com>
2024-12-02 02:48:00 +1030

    Fix: defaultTheme dark

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -20,7 +20,7 @@ export default function Hero() {
       </div>
 
       {/* Background */}
-      <div className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.05] bg-grid-black/[0.2] absolute top-0 left-0 flex items-center justify-center">
+      <div className="h-screen w-full dark:bg-black-100 bg-white-100 dark:bg-grid-white/[0.05] bg-grid-black/[0.2] absolute top-0 left-0 flex items-center justify-center">
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white-100 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
         {/* Add any sentences within <p> to show upon the grid */}
         <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-100 to-neutral-900 py-8"></p>
