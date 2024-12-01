973ca41cc0c43808769c3b1487253703a52964b7
JackyChn <ccc1020048076@gmail.com>
2024-12-02 02:54:32 +1030

    Fix: spinner color

--- app/_components/ui/Spinner.tsx
+++ app/_components/ui/Spinner.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Spinner() {
   return (
     <div
-      className="mx-auto my-20 h-10 w-10 animate-spin rounded-full border-4 border-solid border-blue-button border-t-transparent"
+      className="mx-auto my-20 h-10 w-10 animate-spin rounded-full border-4 border-solid border-purple-500 border-t-transparent"
       style={{
         borderTopColor: "transparent",
         borderRadius: "50%",
