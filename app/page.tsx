4dcb9d0c5a0639fe8d327db67e887e85f050993f
JackyChn <ccc1020048076@gmail.com>
2024-12-02 00:18:28 +1030

    Fix: release Experience and Clients compo

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { navItems } from "@/data";
 // import Grid from "../app/_components/Grid";
 // import Projects from "./_components/Projects";
 // import Clients from "./_components/Clients";
-// import Experience from "./_components/Experience";
+import Experience from "./_components/Experience";
 import Footer from "./_components/Footer";
 
 export default function Home() {
@@ -12,11 +12,11 @@ export default function Home() {
     <main className="relative bg-black-100 text-white-200 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
-        {/* <Hero />
-        <Grid />
-        <Projects />
+        {/* <Hero /> */}
+        {/* <Grid /> */}
+        {/* <Projects /> */}
         <Clients />
-        <Experience /> */}
+        <Experience />
         <Footer />
       </div>
     </main>
